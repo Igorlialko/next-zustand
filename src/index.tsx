95ca28e0e891b4302b7c5ca18be562111d6be266
Igor Lialko <igor.lialko@abz.agency>
2023-05-30 16:47:32 +0300

    feat: init

---
+++ src/index.tsx
@@ -0,0 +1,43 @@
+import React, { ReactNode, createContext, useContext, useRef } from 'react'
+
+import { createStore, useStore as useStoreZustand } from 'zustand'
+import { StateCreator, StoreMutatorIdentifier } from 'zustand/vanilla'
+
+export const createProvider =
+  <T, >() =>
+    <Mos extends [StoreMutatorIdentifier, unknown][] = []>(initializer: StateCreator<T, [], Mos>) => {
+      type TInitialState = Partial<T>;
+      const initializeStore = (initialState: TInitialState) =>
+        createStore<T>()((...args) => ({
+          ...initializer(...args),
+          ...initialState
+        }))
+
+      type StoreType = ReturnType<typeof initializeStore>;
+
+      const ZustandContext = createContext<StoreType | null>(null)
+
+      const StoreProvider = ({
+                               children,
+                               initialState
+                             }: {
+        children: ReactNode;
+        initialState: TInitialState;
+      }) => {
+        const storeRef = useRef<StoreType>()
+        if (!storeRef.current) {
+          storeRef.current = initializeStore(initialState)
+        }
+        return <ZustandContext.Provider value={storeRef.current}>{children}</ZustandContext.Provider>
+      }
+
+      const useStore = <R, >(selector: (state: T) => R, shallow?: (a: R, b: R) => boolean): R => {
+        const store = useContext(ZustandContext)
+
+        if (!store) throw new Error('Store is missing the provider')
+
+        return useStoreZustand(store, selector, shallow)
+      }
+
+      return { Provider: StoreProvider, useStore }
+    }
